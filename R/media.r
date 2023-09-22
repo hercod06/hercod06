34196dc8edc64a003faa5d74cfa84e87aa74866d
Hernando coneo <hernandoconeo1@hotmail.com>
2023-09-22 14:59:14 -0500

    Update media.r

---
+++ R/media.r
@@ -0,0 +1,10 @@
+# Este es un programa en R para calcular la media de una lista de números.
+
+# Crear una lista de números
+numeros <- c(5, 10, 15, 20, 25)
+
+# Calcular la media
+media <- mean(numeros)
+
+# Mostrar el resultado
+cat("La media de los números es:", media, "\n")
