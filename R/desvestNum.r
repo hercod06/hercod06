04f186ca0decd13a9d0e6874070ea7d9881d1ab8
Hernando coneo <hernandoconeo1@hotmail.com>
2023-09-25 12:51:38 -0500

    Update desvestNum.r

---
+++ R/desvestNum.r
@@ -0,0 +1,10 @@
+# Este es un programa en R para calcular la desviación estándar de una lista de números.
+
+# Crear una lista de números
+numeros <- c(10, 15, 20, 25, 30)
+
+# Calcular la desviación estándar
+desviacion_estandar <- sd(numeros)
+
+# Mostrar el resultado
+cat("La desviación estándar de los números es:", desviacion_estandar, "\n")
