9cb57030f6fa1f5f5fb871c0dce169234a26dc42
Hernando coneo <hernandoconeo1@hotmail.com>
2023-09-26 13:23:57 -0500

    Update graficar1.r

---
+++ R/graficar1.r
@@ -0,0 +1,7 @@
+# Este es un programa en R para graficar una serie de datos.
+
+# Crear datos
+datos <- c(10, 15, 20, 25, 30)
+
+# Crear un gráfico de barras
+barplot(datos, main="Gráfico de Barras", xlab="Categorías", ylab="Valores")
