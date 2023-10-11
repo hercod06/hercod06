14e649d5daa5b885232f3351821cf2e5aa68e3a8
Hernando coneo <hernandoconeo1@hotmail.com>
2023-10-11 12:53:06 -0500

    Update webDinamica.php

---
+++ Php/webDinamica.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página Web Dinámica</title>
+</head>
+<body>
+    <h1>Bienvenido a mi página web dinámica</h1>
+    <p>La hora actual es: <?php echo date('H:i:s'); ?></p>
+</body>
+</html>
